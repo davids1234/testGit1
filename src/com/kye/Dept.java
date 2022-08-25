8b47a75b063ac17693abbc47e32cc633f61e56d8
davids1234 <jenuls123@gmail.com>
2022-08-25 15:10:28 +0800

    first Version

---
+++ src/com/kye/Dept.java
@@ -0,0 +1,13 @@
+package com.kye;
+
+public class Dept {
+	
+	String aaa;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("testGit  --Dept");
+	}
+
+}
---
+++ src/com/kye/Employee.java
@@ -0,0 +1,13 @@
+package com.kye;
+
+public class Employee {
+	
+	String aaa;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("testGit --employee");
+	}
+
+}
